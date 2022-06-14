1e117d0fd0ee8950006e4353142211b1f913d871
Annie Nguyen <annienguyen275@gmail.com>
2022-06-14 12:23:40 -0500

    Initial Commit

---
+++ aboutme2.0/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  aboutme2.0
+//
+//  Created by Annie Nguyen on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        fact1.isHidden = true
+        fact2.isHidden = true
+        fact3.isHidden = true
+        fact4.isHidden = true
+        fact5.isHidden = true
+    }
+    @IBOutlet weak var fact1: UILabel!
+    @IBOutlet weak var fact2: UILabel!
+    @IBOutlet weak var fact3: UILabel!
+    @IBOutlet weak var fact4: UILabel!
+    @IBOutlet weak var fact5: UILabel!
+    
+    @IBAction func clickHereButtonTapped(_ sender: UIButton) {
+        fact1.isHidden = false
+        fact2.isHidden = false
+        fact3.isHidden = false
+        fact4.isHidden = false
+        fact5.isHidden = false
+    }
+}
+
